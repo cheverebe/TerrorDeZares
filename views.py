5ba0424610a070cb87272f421ac09e40da41ac7d
cheverebe <arebecchi@devartis.com>
2014-07-20 19:00:55 -0300

    agrupaso por ip

--- views.py
+++ views.py
@@ -9,13 +9,16 @@ class HomeView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
+        ip = get_client_ip(self.request)
+        context["ip"] = ip
         req = Request.objects.create()
         if len(self.request.GET.keys()):
             context['get'] = ", ".join([key + " - " + self.request.GET[key] for key in self.request.GET.keys()])
             req.get = context['get']
         if len(self.request.POST.keys()):
             context['post'] = ", ".join([key + " - " + self.request.POST[key] for key in self.request.POST.keys()])
-            req.post = context['post']
+            req.post = self.request.POST['data']
+            req.get = str(ip)
         req.save()
         return context
 
@@ -47,3 +50,12 @@ class PostFile(CreateView):
 class AllFiles(ListView):
     template_name = 'all_files.html'
     model = PostedFile
+
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
